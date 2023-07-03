975ba485f2246ade14e8d898d3ee70980fab71f2
Tommerty <86270372+tommerty@users.noreply.github.com>
2023-07-03 11:15:30 +0100

    init and copy

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,14 @@
+<script>
+    	import Icon from '@iconify/svelte';
+</script>
+<div class="hero">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-7xl font-bold">GezelBot</h1>
+        <h2 class="text-xl font-bold">is temporarily offline</h2>
+        <p class="py-6">We're hard at work rebuilding the bot from the ground up and improving our features.<br class="mb-4">
+        To stay up to date, we invite you to join our Discord server</p>
+        <a href="https://gezel.io/discord" class="btn btn-primary px-9 py-7 content-center"><Icon icon="radix-icons:discord-logo" width="50" height="50" /></a>
+      </div>
+    </div>
+  </div>
